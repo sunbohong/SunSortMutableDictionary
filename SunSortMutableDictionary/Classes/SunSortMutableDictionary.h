ff8a72639d08478d3b24fecb838f33655aec8176
sunbohong <sunbohong@gmail.com>
2016-09-21 14:09:00 +0800

    Add SunSortMutableDictionary

---
+++ SunSortMutableDictionary/Classes/SunSortMutableDictionary.h
@@ -0,0 +1,15 @@
+//
+//  SunSortMutableDictionary.h
+//  SunSortMutableDictionary
+//
+//  Created by sunbohong on 16/9/21.
+//  Copyright © 2016年 sunbohong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SunSortMutableDictionary : NSDictionary
+
+@property (nonatomic,copy) NSComparator comparator;
+
+@end
---
+++ SunSortMutableDictionary/Classes/SunSortMutableDictionary.m
@@ -0,0 +1,57 @@
+//
+//  SunSortMutableDictionary.m
+//  SunSortMutableDictionary
+//
+//  Created by sunbohong on 16/9/21.
+//  Copyright © 2016年 sunbohong. All rights reserved.
+//
+
+#import "SunSortMutableDictionary.h"
+
+
+@implementation SunSortMutableDictionary {
+    NSMutableDictionary *__dic;
+}
+
++(instancetype)dictionary{
+    return [SunSortMutableDictionary new];
+}
+
+-(instancetype)init{
+    if (self=[super init]) {
+        __dic=[NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (nullable id)objectForKeyedSubscript:(NSString*)key{
+    return __dic[key];
+}
+
+- (void)setObject:(nullable id)obj forKeyedSubscript:(id <NSCopying>)key{
+    __dic[key]=obj;
+}
+
+-(NSUInteger)count{
+    return __dic.count;
+}
+
+-(NSString *)description{
+    return [@"" stringByAppendingFormat:@"%@:%p\n%@",[self class],self,__dic];
+}
+
+-(NSEnumerator *)keyEnumerator{
+    if (!self.comparator) {
+        return __dic.keyEnumerator;
+    }else{
+        NSMutableArray *arr=[NSMutableArray arrayWithArray:__dic.allKeys];
+        [arr sortUsingComparator:self.comparator];
+        return arr.objectEnumerator;
+    }
+}
+
+-(id)objectForKey:(id)aKey{
+    return __dic[aKey];
+}
+
+@end
