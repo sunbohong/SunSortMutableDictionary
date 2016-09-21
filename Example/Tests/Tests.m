e3778ed77bc7b264468904068afd4c5c4de52df3
sunbohong <sunbohong@gmail.com>
2016-09-21 17:37:39 +0800

    Add support for Pod

---
+++ Example/Tests/Tests.m
@@ -0,0 +1,63 @@
+//
+//  SunSortMutableDictionaryTests.m
+//  SunSortMutableDictionaryTests
+//
+//  Created by sunbohong on 09/21/2016.
+//  Copyright (c) 2016 sunbohong. All rights reserved.
+//
+
+@import XCTest;
+
+#include <SunSortMutableDictionary/SunSortMutableDictionary.h>
+
+@interface Tests : XCTestCase
+
+@end
+
+@implementation Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testASC
+{
+    SunSortMutableDictionary *dic = [SunSortMutableDictionary new];
+    dic.comparator = ^(NSString* obj1,NSString* obj2){
+        return  [obj1 compare: obj2];
+    };
+
+    dic[@"a"]  = @"a";
+    dic[@"c"]  = @"c";
+    dic[@"e"]  = @"e";
+    NSData *data=[NSJSONSerialization dataWithJSONObject:dic options:0 error:NULL];
+    NSString *str=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    XCTAssert([str isEqualToString:@"{\"a\":\"a\",\"c\":\"c\",\"e\":\"e\"}"]);
+}
+
+
+- (void)testDesc
+{
+    SunSortMutableDictionary *dic = [SunSortMutableDictionary new];
+    dic.comparator = ^(NSString* obj1,NSString* obj2){
+        return  0 - [obj1 compare: obj2];
+    };
+
+    dic[@"a"]  = @"a";
+    dic[@"c"]  = @"c";
+    dic[@"e"]  = @"e";
+    NSData *data=[NSJSONSerialization dataWithJSONObject:dic options:0 error:NULL];
+    NSString *str=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    XCTAssert([str isEqualToString:@"{\"e\":\"e\",\"c\":\"c\",\"a\":\"a\"}"]);
+}
+
+@end
+
---
+++ SunSortMutableDictionary.podspec
@@ -0,0 +1,12 @@
+Pod::Spec.new do |s|
+s.name = 'SunSortMutableDictionary'
+s.version = '0.4.0'
+s.license = 'MIT'
+s.summary = '可以按照key进行排序的可变数组'
+s.homepage = 'https://github.com/sunbohong/SunSortMutableDictionary'
+s.authors = { '酷酷的哀殿' => 'sunbohong@gmail.com' }
+s.source = { :git => 'https://github.com/sunbohong/SunSortMutableDictionary.git', :tag => s.version }
+
+s.ios.deployment_target = '8.0'
+s.source_files = 'Pod/Classes/**/*'
+end
